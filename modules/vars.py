63e6eb946b42e8c73ae0e254e523c7d11b231654
Tgsamaritan <166801553+Tgsamaritan@users.noreply.github.com>
2024-04-15 10:26:50 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "20242940"
 api_hash = "668e6337b23734c0dd6e433de3ea75d5"
-bot_token = "7087918256:AAHAcDuKubsksgUKsPbGIt5pLuHNDWYR_lo"
+bot_token = "6873193990:AAF8HTCzwxnpZ21EnHn93Lq3r0eZ9rF4xM8"
