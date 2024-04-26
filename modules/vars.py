9b9c402ef8cb9e1c2b08c8fde24f72f60ea8b9fd
Tgsamaritan <166801553+Tgsamaritan@users.noreply.github.com>
2024-04-27 02:32:12 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "26396085"
+api_hash = "391b135bbe30e4303f254996c40f2aac"
+bot_token = "7185358050:AAEoHzvXaSA3V0dbchFlVmX9E7hqUoN_s8Y"
